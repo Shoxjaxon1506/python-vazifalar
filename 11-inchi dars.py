057c6e47ecb835d0e378a30d2e0fefd37037c9ac
Shoxjaxon1506 <shoxjahonmusahonov009@gmail.com>
2026-01-24 15:22:30 +0500

    men kodlarni 10-12inchi darslar joylandi
    men kodlarni 10-darsdan boshlab qoyaman bundan oldingi darslarni saqlab qo'ymaganim uchun

---
+++ 11-inchi dars.py
@@ -0,0 +1,23 @@
+menu=['non','chuchvara','osh','shashlik','lavash','sho\'rva','choy']
+#ovqat= input('Ovqat turini kiriting:')
+#if ovqat.lower() in menu:   
+ #   print('Buyurtmangiz qabul qilindi')
+#else:
+#    print('Afsuski bizda bunday turdagi ovqat yoq!!')
+#buyurtma=input('Ovqat turini kiriting\n:')
+#if buyurtma in menu:
+ #  print(f'Bizning menuda {buyurtma} bor')
+#else:
+  # print(f'Bizning menuda bunday turdagi {buyurtma} ovqati yoq')
+#son= float(input('Istalgan son kiriting:'))
+#son=int(son)
+#ildiz=son**0.5
+#print(f'{son} ning ildizi {ildiz}ga teng')
+#mevalar=['olma','anor','shaftoli','tarvuz','nok','banan']
+#xaridor=input('Qanday turdagi meva sotib olmoqchisiz\n:')
+#if xaridor in mevalar:
+ #   print('Albatta bizda bunday turdagi meva bor')
+#else:
+ #   print('Afsuski biznig do\'konda bunday turdagi meva yoq')
+sinfdoshlar=[]
+for s in range(1.5):   
